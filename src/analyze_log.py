1f35440189fdddf9518734b59dc80f52f4de1851
davi <davijluna@gmail.com>
2023-02-15 18:53:04 -0300

    fix: requisito 1 acrecentando o metodo os para verificação de arquivo e usando o metodo endwith para pegar a extensão

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,4 +1,13 @@
+import csv
+import os.path
+
+
 def analyze_log(path_to_file):
-    if path_to_file != 'csv':
-        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}")
-    # raise NotImplementedError
+
+    if not path_to_file.endswith('.csv'):
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+    if not os.path.exists(path_to_file):
+        raise FileNotFoundError(f"Arquivo inexistente '{path_to_file}'")
+
+
+print(analyze_log("data/orders_1.csv"))
