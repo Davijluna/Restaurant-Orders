373f82c294c8d9dc9e3ccfb7e05f42435ec4e9b9
davi <davijluna@gmail.com>
2023-02-15 14:17:50 -0300

    feat: requisito 1 iniciando a implementação da função analyze_log para verificação de arquivo csv

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,4 @@
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    if path_to_file != 'csv':
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}")
+    # raise NotImplementedError
