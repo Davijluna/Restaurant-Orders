7242a9b691c1a9d0d6994b5e01cd161d0fb72528
davi <davijluna@gmail.com>
2023-02-16 19:12:02 -0300

    feat: requisito 2 implementando class TrackOrders

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,19 +1,26 @@
+from src.analyze_log import (func_quantid_never, func_never_was)
+
+
 class TrackOrders:
     # aqui deve expor a quantidade de estoque
+    def __init__(self) -> None:
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.orders.append((customer, order, day))
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        result = [order[1] for order in self.orders if order[0] == customer]
+        return max(result, key=result.count)
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        return func_quantid_never(customer, self.orders)
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        return func_never_was(customer, self.orders)
 
     def get_busiest_day(self):
         pass
