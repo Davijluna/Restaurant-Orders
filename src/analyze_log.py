b31aa9a16e4196a510b20b97954fc5aade180d2c
davi <davijluna@gmail.com>
2023-02-16 18:23:07 -0300

    feat: requisito 1 implementando as funções para buscar informações e criando arquivo mkt_campaingn.txt

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,6 +1,7 @@
 import csv
 import os.path
-# from collections import Counter
+
+# FUNÇÃO DE LEITURA
 
 
 def read(path_to_file):
@@ -10,6 +11,9 @@ def read(path_to_file):
         for index in conteudos:
             conteudos_list.append(index)
         return conteudos_list
+
+
+# Retorna Qual o prato mais pedido por 'maria'
 
 
 def func_max_orders(client, orders):
@@ -24,8 +28,53 @@ def func_max_orders(client, orders):
     return max(dishes.items(), key=lambda x: x[1])[0]
 
 
-def analyze_log(path_to_file):
+# Retorna Quantas vezes 'arnaldo' pediu 'hamburguer'
 
+def quantit_ordered_dish(custumer, dish, orders):
+    times = {}
+
+    for client, dish, _ in orders:
+        if client == custumer:
+            if dish in times:
+                times[dish] += 1
+            else:
+                times[dish] = 1
+    return times[dish]
+
+# Retorna Quais pratos 'joao' nunca pediu
+
+
+def func_quantid_never(client, orders):
+    all_food = set()
+    ordered_fod = set()
+
+    for customer, dish, _ in orders:
+        all_food.add(dish)
+
+    for customer, dish, _ in orders:
+        if customer == client:
+            ordered_fod.add(dish)
+
+    return all_food - ordered_fod
+
+# Retorna Quais dias 'joao' nunca foi à lanchonete
+
+
+def func_never_was(client_name, orders):
+    all_days = set()
+    days_never_was = set()
+
+    for _, _, weekday in orders:
+        all_days.add(weekday)
+
+    for customer, _, day in orders:
+        if customer == client_name:
+            days_never_was.add(day)
+
+    return all_days - days_never_was
+
+
+def analyze_log(path_to_file):
     if not path_to_file.endswith('.csv'):
         raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
     if not os.path.exists(path_to_file):
@@ -33,8 +82,17 @@ def analyze_log(path_to_file):
     # result = get_unique_analyze(path_to_file)
     orders = read(path_to_file)
     max_orders = func_max_orders("maria", orders)
-    # quantid_never = func_quantid_never()
-    return max_orders
+    times_ordered = quantit_ordered_dish("arnaldo", "hamburguer", orders)
+    quantid_never = func_quantid_never("joao", orders)
+    never_was = func_never_was("joao", orders)
+
+    # return quantid_never
+
+    with open("data/mkt_campaign.txt", "w") as file:
+        file.write(f"{max_orders}\n")
+        file.write(f"{times_ordered}\n")
+        file.write(f"{quantid_never}\n")
+        file.write(f"{never_was}")
 
 
 print(analyze_log("data/orders_1.csv"))
